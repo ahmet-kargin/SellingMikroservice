cfc06ec6dd8bafd4c261adb99dae53df6eaf2548
AhmetKargin <ahmet.kargin@arkel.com.tr>
2024-08-27 08:49:39 +0300

    Event Bus Uptade

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace EventBus.Base.Events;
+
+public class IntegrationEvent
+{
+    [JsonProperty]
+    public Guid Id { get; private set; }
+    [JsonProperty]
+    public DateTime CreatedDate { get; private set; }
+
+    [JsonConstructor]
+    public IntegrationEvent(Guid id, DateTime createdDate)
+    {
+        CreatedDate = createdDate;
+        Id = id;
+    }
+     
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CreatedDate = DateTime.Now;
+    }
+}
