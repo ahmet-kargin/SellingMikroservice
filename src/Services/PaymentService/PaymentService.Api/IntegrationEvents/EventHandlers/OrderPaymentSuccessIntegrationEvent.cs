70213753c5ac9d0666897ab4c06815c36bfb3faa
Asiye <Asiye@DESKTOP-L79HD2G>
2024-09-13 17:42:30 +0300

    Payment Service completed and tested.

---
+++ src/Services/PaymentService/PaymentService.Api/IntegrationEvents/EventHandlers/OrderPaymentSuccessIntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿using EventBus.Base.Events;
+
+namespace PaymentService.Api.IntegrationEvents.EventHandlers;
+
+public class OrderPaymentSuccessIntegrationEvent : IntegrationEvent
+{
+    public Guid OrderId { get; }
+
+    public OrderPaymentSuccessIntegrationEvent(Guid orderId) => OrderId = orderId;
+}
--- src/Services/PaymentService/PaymentService.Api/Program.cs
+++ src/Services/PaymentService/PaymentService.Api/Program.cs
@@ -1,3 +1,11 @@
+using EventBus.Base.Abstraction;
+using EventBus.Base;
+using EventBus.Factory;
+using PaymentService.Api.IntegrationEvents.EventHandlers;
+using RabbitMQ.Client;
+using static EventBus.Base.EventBusConfig;
+using PaymentService.Api.IntegrationEvents.Events;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +15,36 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddTransient<OrderStartedIntegrationEventHandler>();
+
+builder.Services.AddSingleton<IEventBus>(sp =>
+{
+    EventBusConfig config = new()
+    {
+        ConnectionRetryCount = 5,
+        EventNameSuffix = "IntegrationEvent",
+        SubscriberClientAppName = "PaymentService",
+        EventBusTypes = EventBusType.RabbitMQ,
+        Connection = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            Port = 15672,
+            UserName = "guest",
+            Password = "guest"
+        },
+        //Connection = new ConnectionFactory()
+        //{
+        //    HostName = "c_rabbitmq"
+        //}
+    };
+
+    return EventBusFactory.Create(config, sp);
+});
+builder.Services.AddLogging(configure =>
+{
+    configure.AddConsole();
+    configure.AddDebug();
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,6 +52,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var eventBus = scope.ServiceProvider.GetRequiredService<IEventBus>();
+    eventBus.Subscribe<OrderStartedIntegrationEvent, OrderStartedIntegrationEventHandler>();
 }
 
 app.UseHttpsRedirection();
