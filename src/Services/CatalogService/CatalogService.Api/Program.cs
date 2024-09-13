a88c7e2af34fdc7cd95aed5c696aa17d1e4082ad
Asiye <Asiye@DESKTOP-L79HD2G>
2024-09-13 12:32:52 +0300

    Catalog Services Updated

--- src/Services/CatalogService/CatalogService.Api/Program.cs
+++ src/Services/CatalogService/CatalogService.Api/Program.cs
@@ -1,25 +1,78 @@
+using CatalogService.Api.Extensions;
+using CatalogService.Api.Infrastructure;
+using CatalogService.Api.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuration is accessible through builder.Configuration
+var configuration = builder.Configuration;
 
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogService.Api", Version = "v1" });
+});
+
+builder.Services.Configure<CatalogSettings>(configuration.GetSection("CatalogSettings"));
+
+// Extension methods for configuring services
+builder.Services.ConfigureDbContext(configuration);
+//builder.Services.ConfigureConsul(configuration);
 
 var app = builder.Build();
+
+// Database migration and seeding
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<CatalogContext>();
+        var env = services.GetRequiredService<IWebHostEnvironment>();
+        var logger = services.GetRequiredService<ILogger<CatalogContextSeed>>();
+
+        // Apply database migrations
+        context.Database.Migrate();
+
+        // Seed data if necessary
+        CatalogContextSeed.SeedAsync(context, env, logger).Wait();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CatalogService.Api v1"));
 }
 
 app.UseHttpsRedirection();
 
+// Static file configuration
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "Pics")),
+    RequestPath = "/pics"
+});
+
+app.UseRouting();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Consul registration
+//app.RegisterWithConsul(app.Lifetime, configuration);
 
 app.Run();
