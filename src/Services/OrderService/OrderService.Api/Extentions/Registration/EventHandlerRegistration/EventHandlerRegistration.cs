1a46e274c7a07dddf7596b15d1f8388cd1a14603
Asiye <Asiye@DESKTOP-L79HD2G>
2024-09-18 19:17:57 +0300

    OrderService.Api completed

---
+++ src/Services/OrderService/OrderService.Api/Extentions/Registration/EventHandlerRegistration/EventHandlerRegistration.cs
@@ -0,0 +1,13 @@
+﻿using OrderService.Api.IntegrationEvents.EventHandlers;
+
+namespace OrderService.Api.Extentions.Registration.EventHandlerRegistration;
+
+public static class EventHandlerRegistration
+{
+    public static IServiceCollection ConfigureEventHandlers(this IServiceCollection services)
+    {
+        services.AddTransient<OrderCreatedIntegrationEventHandler>();
+
+        return services;
+    }
+}
--- src/Services/OrderService/OrderSevice.Infrastructure/ServiceRegistration.cs
+++ src/Services/OrderService/OrderSevice.Infrastructure/ServiceRegistration.cs
@@ -35,4 +35,3 @@ public static class ServiceRegistration
         return services;
     }
 }
-}
